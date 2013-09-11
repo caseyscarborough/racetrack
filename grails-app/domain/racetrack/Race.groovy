6e20bc0d122f053408e98058f1fc1f10cd40fc41
Casey Scarborough <caseyscarborough@gmail.com>
2013-09-11 13:13:20 -0400

    Add some validation constraints to Race.groovy.

--- grails-app/domain/racetrack/Race.groovy
+++ grails-app/domain/racetrack/Race.groovy
@@ -2,14 +2,25 @@ package racetrack
 
 class Race {
 
+    // Domain class attributes
     String name
     Date startDate
     String city
     String state
     BigDecimal distance
     BigDecimal cost
-    Integer maxRunners = 100000
+    Integer maxRunners
 
-    static constraints = {}
+    static constraints = {
+        // Set the order for the fields in the scaffolded view,
+        // in addition to setting constraints for the fields.
+        name(blank:false, maxSize:50)
+        startDate()
+        city()
+        state(inList: ["GA", "NC", "SC", "VA"])
+        distance(min:0.0)
+        cost(min:0.0, max:0.0)
+        maxRunners(min:0, max:10000)
+    }
 
 }
